4b2f2726a98545c503bcb5f8e7e5cf9b597c621c
Serena He <SHe@verathon.com>
2019-12-03 17:45:29 -0800

    Dev #1 Add grid and menu buttons

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -7,43 +7,69 @@
  */
 
 import java.awt.*;
+import java.lang.management.MemoryNotificationInfo;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class GameOfLife {
 
     // -----GUI-----
     // Dimensions
-    static final Dimension SCREEN_DIM = Toolkit.getDefaultToolkit().getScreenSize();
-    static final int GAME_WIDTH = (int)(SCREEN_DIM.getWidth()/2.5);
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH*0.8); // 5:4 ratio
+    static final int GAME_WIDTH = 800;
+    static final int GAME_HEIGHT = 700; // 10:9 ratio
+    static final int GRID_WIDTH = 650;
+    static final int MENU_WIDTH = 100;
+
     static final Dimension GAME_DIMENSION = new Dimension(GAME_WIDTH, GAME_HEIGHT);
-    static final Dimension GRID_DIMENSION = new Dimension(GAME_HEIGHT, GAME_HEIGHT);
-    static final int MENU_WIDTH = (int)(GAME_WIDTH*0.2);
+    static final Dimension GRID_DIMENSION = new Dimension(GRID_WIDTH, GRID_WIDTH);
     static final Dimension MENU_DIMENSION = new Dimension(MENU_WIDTH, GAME_HEIGHT);
-    // Frame
+    // Frames
     private JFrame gameFrame = new JFrame("Conway's Game of Life - Copycat");
+    private JFrame blurbFrame = new JFrame("Blurb");
     // Panels
     private JPanel gridPanel = new JPanel();
     private JPanel menuPanel = new JPanel();
+    private JPanel finalPanel = new JPanel();
+    private JPanel blurbPanel = new JPanel();
+    // Buttons
+    private JButton startStopButton = new JButton();
+    private JButton clearButton = new JButton();
+    private JButton blurbButton = new JButton();
+    // Labels
+    private JLabel blurbLabel = new JLabel();
 
     // -----GAME-----
-    static final int ROWS = 30;
-    static final int COLS = 30;
+    static final int ROWS = 50;
+    static final int COLS = 50;
     LifeCell[][] lifeGrid;
 
     public GameOfLife() {
         /**
          * Constructor for a new Game of Life
          */
-        // Add life cells to grid
+
+        // -----GRID-----
         lifeGrid = new LifeCell[ROWS][COLS];
         for (int row=0; row < ROWS; row++) {
             for (int col=0; col < COLS; col++) {
                 LifeCell cell = new LifeCell();
                 lifeGrid[row][col] = cell;
+                cell.addMouseListener(new LifeMouseListener(this));
             }
         }
+        // -----MOUSE-----
+        startStopButton.addMouseListener(new LifeMouseListener(this));
+        clearButton.addMouseListener(new LifeMouseListener(this));
+        blurbButton.addMouseListener(new LifeMouseListener(this));
 
+        // -----BLURB-----
+        blurbFrame.setSize(100, 200);
+        blurbPanel.setSize(100, 200);
+        blurbLabel.setText("Testing\nTesting");
+        blurbPanel.add(blurbLabel);
+        blurbFrame.add(blurbPanel);
+
+        // Start
         startGame();
     }
 
@@ -53,9 +79,10 @@ public class GameOfLife {
          * @return Nothing.
          */
 
-        // Grid
+        // -----GRID-----
         gridPanel.setPreferredSize(GRID_DIMENSION);
         gridPanel.setLayout(new GridLayout(ROWS, COLS, 0, 0));
+        gridPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         // Add life cells to panel
         for (int row=0; row < ROWS; row++) {
             for (int col=0; col < COLS; col++) {
@@ -63,22 +90,80 @@ public class GameOfLife {
             }
         }
 
-        // Menu
-        menuPanel.setPreferredSize(MENU_DIMENSION);
+        // -----MENU-----
+        // Panel
+        menuPanel.setSize(MENU_DIMENSION);
+        menuPanel.setBackground(Color.WHITE);
         menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
+        menuPanel.setBorder(new EmptyBorder(new Insets(50, 10, 50, 10)));
+        menuPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        // Button name
+        startStopButton.setName("start");
+        clearButton.setName("clear");
+        blurbButton.setName("blurb");
+        // Button text
+        startStopButton.setText("Start");
+        clearButton.setText("Clear");
+        blurbButton.setText("Blurb");
+        // Button size
+        startStopButton.setSize(MENU_WIDTH, 20);
+        clearButton.setSize(MENU_WIDTH, 20);
+        blurbButton.setSize(MENU_WIDTH, 20);
+        // Button alignment
+        startStopButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        clearButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        blurbButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        blurbButton.setAlignmentY(Component.BOTTOM_ALIGNMENT );
+        // Add buttons to panel
+        menuPanel.add(startStopButton);
+        menuPanel.add(Box.createRigidArea(new Dimension(MENU_WIDTH, 10)));
+        menuPanel.add(clearButton);
+        menuPanel.add(Box.createRigidArea(new Dimension(MENU_WIDTH, 50)));
+        menuPanel.add(blurbButton);
 
-        // Add components
-        gameFrame.add(gridPanel);
-        gameFrame.add(menuPanel);
-
+        // -----FINAL PANEL-----
+        finalPanel.add(gridPanel);
+        finalPanel.add(menuPanel);
+        finalPanel.setLayout(new BoxLayout(finalPanel, BoxLayout.X_AXIS));
         // Start frame
+        gameFrame.add(finalPanel);
+        gameFrame.setLayout(new FlowLayout());
         gameFrame.setSize(GAME_DIMENSION);
+        gameFrame.pack();
         gameFrame.setResizable(false);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setLocationRelativeTo(null);
         gameFrame.setVisible(true);
     }
 
+    void clearGrid() {
+        /**
+         * Set all cells to dead
+         * @return Nothing.
+         */
+        for (int row=0; row < ROWS; row++) {
+            for (int col=0; col < COLS; col++) {
+                lifeGrid[row][col].setDead();
+            }
+        }
+    }
+
+    void blurbClicked() {
+        /**
+         * Open blurb window
+         * @return Nothing.
+         */
+        blurbFrame.pack();
+        blurbFrame.setResizable(false);
+        blurbFrame.setLocationRelativeTo(null);
+        blurbFrame.setVisible(true);
+    }
+
     public static void main(String[] args) {
+        System.out.println("Game dimension " + GAME_DIMENSION);
+        System.out.println("Grid dimension " + GRID_DIMENSION);
+        System.out.println("Cell dimension " + LifeCell.CELL_DIMENSION);
+
         GameOfLife newGame = new GameOfLife();
     }
 }
--- src/LifeCell.java
+++ src/LifeCell.java
@@ -11,14 +11,17 @@ import java.awt.*;
 public class LifeCell extends JButton {
 
     // -----DIMENSIONS-----
-    private final int CELL_WIDTH = GameOfLife.GAME_WIDTH/GameOfLife.COLS;
-    private final Dimension CELL_DIMENSION = new Dimension(CELL_WIDTH, CELL_WIDTH);
+    static final int CELL_WIDTH = GameOfLife.GRID_WIDTH/GameOfLife.COLS;
+    static final Dimension CELL_DIMENSION = new Dimension(CELL_WIDTH, CELL_WIDTH);
 
     // -----ATTRIBUTES-----
     private boolean isAlive;
     private boolean willBeAlive;
 
-    public LifeCell() {
+    LifeCell() {
+        /** CONSTRUCTOR
+         *  Construct a new LifeCell
+         */
         // Attributes
         isAlive = false;
         willBeAlive = false;
@@ -26,4 +29,25 @@ public class LifeCell extends JButton {
         setSize(CELL_DIMENSION);
         setBackground(Color.GRAY);
     }
+
+    public void setAlive() {
+        isAlive = true;
+        setBackground(Color.YELLOW);
+    }
+
+    public void setDead() {
+        isAlive = false;
+        setBackground(Color.GRAY);
+    }
+
+    public void cellClicked() {
+        if (isAlive) {
+            setDead();
+        }
+        else {
+            setAlive();
+        }
+    }
+
+
 }
